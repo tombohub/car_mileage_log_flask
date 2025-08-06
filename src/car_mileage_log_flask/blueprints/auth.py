b93e24e1f6911157789805d9b729a5d802bc3f2e
me <tkutlesa@gmail.com>
2025-08-06 01:17:02 -0400

    logged in

--- src/car_mileage_log_flask/blueprints/auth.py
+++ src/car_mileage_log_flask/blueprints/auth.py
@@ -13,7 +13,7 @@ auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 def login_required(view):
     @wraps(view)
     def wrapped_view(*args, **kwargs):
-        if not session["logged_in"]:
+        if not session.get("logged_in"):
             return redirect(url_for("auth.login"))
         return view(*args, **kwargs)
 
